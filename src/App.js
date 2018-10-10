fe2cb879540cfdec50342b4f9bc189328b3d8c49
Unknown <cmveit@web.de>
2018-10-10 08:43:16 +0200

    Open file commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  // constructor(props){
+  //     super(props);
+  //
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Just a Test.
+            {/* <FileInput /> */}
+            {ImportFromFileBodyComponent()}
           </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
   }
 }
 
+
+// class FileInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.fileInput = React.createRef();
+//     this.fileReader = new FileReader();
+//   }
+//
+//
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     this.fileReader.readAsText(this.fileInput.current.files[0]);
+//     alert(
+//       `Selected file - ${
+//         this.fileReader.result
+//       }`
+//     );
+//   }
+//
+//   render(){
+//     return(
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Upload file:
+//           <input type="file" ref={this.fileInput} />
+//         </label>
+//         <br />
+//         <button type="submit">Submit</button>
+//       </form>
+//     );
+//   }
+// }
+
+
+const ImportFromFileBodyComponent = () => {
+    let fileReader;
+
+    const handleFileRead = (e) => {
+        const content = fileReader.result;
+        console.log(content);
+        alert(`Selected file - ${content}`);
+        return(
+            content
+          );
+    };
+
+    const handleFileChosen = (file) => {
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file);
+    };
+
+    return <div className='upload-expense'>
+        Open File:<br/>
+        <input type='file'
+               id='file'
+               className='input-file'
+               accept='.txt'
+               onChange={e => handleFileChosen(e.target.files[0])}
+        />
+    </div>;
+};
+
+
+
+
+// function handleFileSelect(evt) {
+//     const f = evt.target.file;
+//     const output = [];
+//     output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+//       f.size, ' bytes, last modified: ',
+//       f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+//       '</li>'
+//     );
+//     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+//   }
+//
 export default App;
